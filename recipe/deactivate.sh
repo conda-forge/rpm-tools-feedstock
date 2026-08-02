33f913bf5ad5f7bdde8c72a648da9f442954d4a0
Geert Geurts <geert@verweggistan.eu>
2026-08-02 16:49:38 +0200

    Revert "Add setting of RPM_CONFIGDIR"
    This reverts commit acefd311aa643679d7c52b88d7fb55bcfabf0e2c.

--- recipe/deactivate.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-# Restore previous value if one existed before activation.
-if [ -n "${CONDA_BACKUP_RPM_TOOLS_RPM_CONFIGDIR+x}" ]; then
-    export RPM_CONFIGDIR="${CONDA_BACKUP_RPM_TOOLS_RPM_CONFIGDIR}"
-    unset CONDA_BACKUP_RPM_TOOLS_RPM_CONFIGDIR
-else
-    unset RPM_CONFIGDIR
-fi
